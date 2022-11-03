b19a992a3803b0996ac4378787c91a74847b86cf
carton04 <114985496+carton04@users.noreply.github.com>
2022-11-04 06:06:38 +0900

    activehash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,34 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :cost_id
+    validates :prefecture_id
+    validates :duration_id
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :cost_id
+    validates :prefecture_id
+    validates :duration_id
+  end
+
+  validates_inclusion_of :price, in: 300..9999999
+  validates :price, numericality: { with: /\A[0-9]+\z/}
+
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :duration
+
 end
