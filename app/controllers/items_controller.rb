30ac1e4b40faf90a9b553bb852be28dac945daf6
carton04 <114985496+carton04@users.noreply.github.com>
2022-11-04 05:22:33 +0900

    画像投稿機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :cost_id, :prefecture_id, :duration_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
